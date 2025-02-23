413a0688e0eae1e904dab4bb758ddfeb4f5e1b67
Krisztián Szabó <sz.krisztian96@gmail.com>
2025-02-23 07:53:19 +0100

    Can extract index information (although not perfectly) and construct faces now!

--- src/main.cpp
+++ src/main.cpp
@@ -30,8 +30,11 @@ int main(int argc, char* argv[]) {
 
     const auto root = Chunk::parse_root(bytes);
     static constexpr std::uint32_t mesh_chunk_type = 0x134B01;
+    static constexpr std::uint32_t index_chunk_type = 0x134B03;
     const auto mesh_chunks = ChunkUtils::get_all_with_type(*root, mesh_chunk_type);
+    const auto index_chunks = ChunkUtils::get_all_with_type(*root, index_chunk_type);
     std::cout << "Found " << mesh_chunks.size() << " mesh chunks." << std::endl;
+    std::cout << "Found " << index_chunks.size() << " index chunks." << std::endl;
 
     // Create the out folder if it does not exist yet
     try {
@@ -61,8 +64,6 @@ int main(int argc, char* argv[]) {
         }
         // The vertex layout starts with position data that is 12 bytes before the first normals
         const auto starting_idx = first_normal_offset.value() - 12;
-
-        // std::size_t vertex_idx = 0;
         for (std::size_t i = starting_idx; i <= data.size() - stride; i += stride) {
             float vertex_x = *reinterpret_cast<const float*>(&data.at(i));
             float vertex_y = *reinterpret_cast<const float*>(&data.at(i + 4));
@@ -77,18 +78,23 @@ int main(int argc, char* argv[]) {
             float u = *reinterpret_cast<const float*>(&data.at(i + 28));
             float v = *reinterpret_cast<const float*>(&data.at(i + 32));
             texture_coordinates.emplace_back(u, v);
-
-            // For every 3rd vertex create a face
-            /* TODO: Skip faces for now as we do not know yet how the geometry data is indexed
-            if (++vertex_idx % 3 == 0) {
-                faces.emplace_back(
-                    std::array<std::size_t, 3>{ vertex_idx - 2, vertex_idx - 1, vertex_idx },
-                    std::array<std::size_t, 3>{ vertex_idx - 2, vertex_idx - 1, vertex_idx },
-                    std::array<std::size_t, 3>{ vertex_idx - 2, vertex_idx - 1, vertex_idx }
-                );
-            }
-            */
         }
+
+        // Face data is stored in the index chunks in 16 bit integers, we read out 3 at a time to construct a face
+        const auto& index_chunk = *index_chunks[mesh_chunk_idx];
+        // TODO: That -6 should not be necessary. It seems like number of 16 bit ints are not divisible by 3 which is weird?!
+        for (std::size_t i = 0; i < index_chunk.data.size() - 6; i += 6) {
+            // Adding 1 to all of these due to OBJ indexing from 1
+            std::uint16_t first_index = *reinterpret_cast<const std::uint16_t*>(&index_chunk.data.at(i)) + 1;
+            std::uint16_t second_index = *reinterpret_cast<const std::uint16_t*>(&index_chunk.data.at(i + 2)) + 1;
+            std::uint16_t third_index = *reinterpret_cast<const std::uint16_t*>(&index_chunk.data.at(i + 4)) + 1;
+            faces.emplace_back(
+                std::array<std::size_t, 3>{ first_index, second_index, third_index },
+                std::array<std::size_t, 3>{ first_index, second_index, third_index },
+                std::array<std::size_t, 3>{ first_index, second_index, third_index }
+            );
+        }
+
         std::filesystem::path file_path("out/" + std::to_string(mesh_chunk_idx++) + ".obj");
         std::cout << "Writing '" << file_path.string() << "'..." << std::endl;
         std::ofstream file_handle(file_path);
